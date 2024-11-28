c5eb8b3d43d6d1e6843eef80dccd0d67c19fd955
crakton <redemptionjonathan@outlook.com>
2024-11-28 19:59:39 +0100

    feat: Add related entrities to support the api database and migration setups api configs database seeders storage mechanisms bootsrap

---
+++ database/migrations/2024_11_27_194033_create_fees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('fee_type');
+            $table->string('fee_name');
+            $table->integer('fee_amount');
+            $table->string('fee_status');
+            $table->json('fee_metadata');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fees');
+    }
+};
---
+++ database/migrations/2024_11_27_194033_create_taxes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('tax_type');
+            $table->string('tax_name');
+            $table->integer('tax_year');
+            $table->integer('tax_amount');
+            $table->string('tax_status');
+            $table->integer('gross_income');
+            $table->integer('tax_rate');
+            $table->json('tax_metadata');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('taxes');
+    }
+};
