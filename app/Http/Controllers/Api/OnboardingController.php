a122730e925e0be095abe72b077e350121623166
crakton <redemptionjonathan@outlook.com>
2024-12-27 22:00:34 +0100

    chore: loads upload images from buckets

--- app/Http/Controllers/Api/OnboardingController.php
+++ app/Http/Controllers/Api/OnboardingController.php
@@ -13,17 +13,17 @@ class OnboardingController extends BaseController
 				[
 					'title' => 'AI to simplify your tax journey',
 					'description' => 'New to taxes and fees management? Our AI will guide you every step of the way.',
-					'image_url' => 'https://sharedby.blomp.com/ce4c1d',
+					'image_url' => 'https://cloud.appwrite.io/v1/storage/buckets/676f130100315c886d84/files/676f13a90012a577dc4e/view?project=676f12d0003a9fc65124&project=676f12d0003a9fc65124&mode=admin',
 				],
 				[
 					'title' => 'Pay taxes and fees as simple as ABC',
 					'description' => 'Easily manage your taxes and fees to government and companies from one platform.',
-					'image_url' => 'https://sharedby.blomp.com/rkjKyG',
+					'image_url' => 'https://cloud.appwrite.io/v1/storage/buckets/676f130100315c886d84/files/676f1402003c43c72296/view?project=676f12d0003a9fc65124&project=676f12d0003a9fc65124&mode=admin',
 				],
 				[
 					'title' => 'Tax Education ',
 					'description' => 'Get realtime updates on tax laws, governement fess, and private companies deligations.',
-					'image_url' => 'https://sharedby.blomp.com/0A6Gh5',
+					'image_url' => 'https://cloud.appwrite.io/v1/storage/buckets/676f130100315c886d84/files/676f1432001a4aaf9f62/view?project=676f12d0003a9fc65124&project=676f12d0003a9fc65124&mode=admin',
 				],
 			];
 			return $this->sendResponse($onboarding, 'Onboarding data');
